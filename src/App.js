1f2a4df5f36fe42b4ad7ec8e519606b04d20b93c
kunal <kunalbankoti9@gmail.com>
2022-07-17 11:28:08 +0530

    initialize all dice with random number

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import Die from './components/Die';
 
 export default function App() {
 
-    const dies = [];
-    for (let i = 0; i < 10; i++) {
-        dies.push(<Die key={i} value={i} />)
+    function allNewDice() {
+        const arr = []
+        for (let i = 0; i < 10; i++)
+            arr.push({
+                id: i,
+                value: Math.ceil(Math.random() * 6)
+            })
+        return arr;
     }
+    const dice = allNewDice().map(obj => <Die key={obj.id} value={obj.value} />)
     return (
         <div>
             <main>
                 <div className="dice-container">
-                    {dies}
+                    {dice}
                 </div>
             </main>
 
