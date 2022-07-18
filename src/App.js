44916ab6f1935795686c63790a56aa6cd7a908f9
kunal <kunalbankoti9@gmail.com>
2022-07-18 10:31:42 +0530

    Add hold functionality to die
    when clicked, die holds to fixed value. Clicking again will call off the
    'hold'

--- src/App.js
+++ src/App.js
@@ -3,27 +3,43 @@ import Die from './components/Die';
 
 export default function App() {
     const [allDice, setDice] = React.useState(allNewDice)
+
     function allNewDice() {
         const arr = []
         for (let i = 0; i < 10; i++)
             arr.push({
                 id: i,
-                value: Math.ceil(Math.random() * 6)
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false
             })
         return arr;
     }
 
-    function diceRoll() {
-        setDice(allNewDice)
+    function rollDice() {
+        setDice(prevAllDice => prevAllDice.map(die => !die.isHeld ?
+            { ...die, value: Math.ceil(Math.random() * 6) } : die))
     }
-    const dice = allDice.map(data => <Die key={data.id} value={data.value} />)
+
+
+    function holdDice(id) {
+        setDice(prevAllDice => prevAllDice.map(die =>
+            die.id === id ? { ...die, isHeld: !die.isHeld } : die))
+
+    }
+
+    const dice = allDice.map(data => <Die
+        key={data.id}
+        value={data.value}
+        isHeld={data.isHeld}
+        onclick={() => holdDice(data.id)} />)
+
     return (
         <div>
             <main>
                 <div className="dice-container">
                     {dice}
                 </div>
-                <button className="roll" onClick={diceRoll}>Roll</button>
+                <button className="roll" onClick={rollDice}>Roll</button>
             </main>
 
         </div>
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 export default function Die(props) {
+    const holdStyle = {
+        backgroundColor: props.isHeld ? "#59e391" : "white"
+    }
     return (
-        <div className="die">
-            <h1 className="die--num">
+        <div className="die" style={holdStyle}>
+            <h1 className="die--num" onClick={props.onclick}>
                 {props.value}
             </h1>
         </div>
